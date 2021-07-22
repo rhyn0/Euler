2546d1b240bd8f76def9749e45afc65add4c9b3b
Ryan <ryan.ozawa21@gmail.com>
2021-07-22 14:34:24 -0700

    work on C directory

--- C/euler.c
+++ C/euler.c
@@ -1,48 +1,9 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "euler.h"
-
-void showUsage()
-{
-    printf("main [-h] problem_num [n]\n");
-}
-
-void showHelp()
-{
-    printf("Euler Problems:\n\t1 - Sum of multiples of 3 and 5, default 1000\n\t"
-           "2 - Sum of even Fibonacci, default 4 million\n\t"
-           "3 - Largest Prime factor of N, default 600851475143\n\t"
-           "4 - Largest Palindrome Product of 3 digit numbers\n\t"
-           "5 - Smallest multiple of all numbers 1 to n, default 20.\n");
-    exit(0);
-}
-
-int checkArgs(int argc, char **argv, int *n)
-{
-    int i;
-    if (argc == 1 || argc > 3)
-    {
-        fprintf(stderr, "No Euler function specified.\n");
-        showUsage();
-        exit(-1);
-    }
-    // if help flag is suppplied, print help and then quit
-    for (i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-h") == 0)
-            showHelp();
-    }
-    // if user passes additional args, set. Otherwise set to -1
-    if (argc > 2)
-        *n = atoi(argv[2]);
-    else
-        *n = -1;
-    return atoi(argv[1]);
-}
 
 bool isPrime(int num)
 {
@@ -57,30 +18,6 @@ bool isPrime(int num)
     return true;
 }
 
-int main(int argc, char **argv)
-{
-    int prob, addlArgs;
-    prob = checkArgs(argc, argv, &addlArgs);
-    switch (prob)
-    {
-    case 1:
-        printf("Euler 1 is %d\n", euler1(addlArgs));
-        break;
-
-    case 2:
-        printf("Euler 2 is %d\n", euler2(addlArgs));
-        break;
-
-    case 3:
-        printf("Euler 3 is %d\n", euler3(addlArgs));
-        break;
-
-    default:
-        printf("That problem is not available yet.");
-        break;
-    }
-}
-
 /**
  * @brief Multiples of 3 and 5. Sum of multiples of 3 or 5 under n
  * 
@@ -92,10 +29,10 @@ int main(int argc, char **argv)
  */
 int euler1(int n)
 {
-    int sum = 0, i;
     // check if args were passed, if not - default
-    if (n == -1)
+    if (n < 0)
         n = 1000;
+    int sum = 0, i;
     for (i = 3; i < n; i += 3)
         sum += i;
     for (i = 5; i < n; i += 5)
@@ -105,15 +42,142 @@ int euler1(int n)
     }
     return sum;
     // math version
-    // return (int)((n / 3) / 2) * (3 + n - (n % 3)) + (n / 5) / 2 * ((n - n % 5) + 5) - (n / 15) / 2 * ((n - n % 15) + 15);
+    // can not include the n term, supposed to be strictly less than n
+    // n -= 1;
+    // return (int)(floor(n / 3) / 2 * (3 + n - (n % 3)) + floor(n / 5) / 2 * ((n - n % 5) + 5) - floor(n / 15) / 2 * ((n - n % 15) + 15));
 }
 
+/**
+ * @brief Sum of Even Fibonacci numbers
+ * 
+ * Create each term in the fibonacci sequence under the value of n
+ * Add to the running sum only terms of that are even, where 0th and 1st term are 1.
+ * 
+ * @param num limit to sum under
+ * @return int 
+ */
 int euler2(int num)
 {
-    return -1;
+    // check for default arg case
+    if (num < 0)
+        num = 4000000;
+    // int t0 = 1, t1 = 1;
+    // int evenTerm = t0 + t1, sum = 0;
+    // while (evenTerm < num)
+    // {
+    //     sum += evenTerm;
+    //     t0 = t1 + evenTerm;
+    //     t1 = t0 + evenTerm;
+    //     evenTerm = t0 + t1;
+    // }
+    // return sum;
+    /**
+     * Math version
+     * 
+     * If we look at first few terms of Fibonacci sequence, adding in 'X'
+     *      1 '2' 3 5 '8' 13 21 '34'
+     *      2 8 34 -> leads us to believe that E(n) = 4 * E(n - 1) + E(n - 2)
+     * This breaks down into F(n) = 4 * F(n - 3) + F(n - 6) which does hold
+     */
+    int t0 = 0, t1 = 0;
+    int sum = 0, c = 2;
+    while (c < num)
+    {
+        sum += c;
+        t0 = t1;
+        t1 = c;
+        c = 4 * t1 + t0;
+    }
+    return sum;
 }
 
-int euler3(int num)
+/**
+ * @brief Largest Prime Factor of num
+ * 
+ * Doing prime factorization on n directly to find the largest prime factor
+ * Starting at divisor = 2, divide num until num mod divisor != 0, then increase divisor by 1 
+ * 
+ * @param num number to find the largest factor of
+ * @return int 
+ */
+// TODO: test case 60085147514312458
+long int euler3(long long int num)
 {
-    return -1;
+    // check for default args
+    if (num < 0)
+        num = 600851475143;
+    long long int largestFactor = 0, divisor = 2;
+    while (divisor * divisor <= num)
+    {
+        if (num % divisor == 0)
+        {
+            num /= divisor;
+            largestFactor = divisor;
+        }
+        else
+        {
+            divisor += 1;
+        }
+    }
+    return (num > largestFactor) ? num : largestFactor;
+}
+
+/**
+ * @brief Returns the value of an int if read backwards
+ * 
+ * This was implemented as opposed to converting to string and reversing that.
+ * 
+ * @param n number to reverse
+ * @return int 
+ */
+int reverseNum(int n)
+{
+    int rev = 0;
+    while (n > 0)
+    {
+        rev = 10 * rev + n % 10;
+        n /= 10;
+    }
+    return rev;
+}
+
+/**
+ * @brief Largest Palindrome Product of 2 n digit numbers
+ * 
+ * A palindrome is one that is equivalent reading forwards and backwards.
+ * Take 2 n digit integers, multiply them together, see if its a palindrome.
+ * Return the largest palindrome found.
+ * 
+ * @param n length of numbers to multiply, n = 3 means [100, 999]
+ * @return int 
+ */
+int euler4(int n)
+{
+    if (n < 0)
+        n = 3;
+    int biggest = 0, i, j, temp;
+    int start = pow(10, n) - 1, end = pow(10, n - 1);
+    for (i = start; i >= end; i--)
+    {
+        // only need to compute each pair once
+        // e.g. 999 * 100 == 100 * 999
+        for (j = start; j >= i; j--)
+        {
+            temp = i * j;
+            // dont bother checking any numbers under biggest
+            if (temp <= biggest)
+            {
+                break;
+            }
+            else if (reverseNum(temp) == temp)
+            {
+                biggest = temp;
+            }
+        }
+    }
+    return biggest;
+}
+
+int euler5(int num)
+{
 }
---
+++ C/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "euler.h"
+
+void showUsage()
+{
+    printf("main [-h]\n");
+    printf("main problem_num [n]\n");
+}
+
+void showHelp()
+{
+    printf("Euler Problems:\n\t"
+           "1 - Sum of multiples of 3 and 5, default 1000\n\t"
+           "2 - Sum of even Fibonacci, default 4 million\n\t"
+           "3 - Largest Prime factor of N, default 600851475143\n\t"
+           "4 - Largest Palindrome Product of 3 digit numbers\n\t"
+           "5 - Smallest multiple of all numbers 1 to n, default 20.\n");
+    exit(0);
+}
+
+int checkArgs(int argc, char **argv, long long int *n)
+{
+    int i;
+    if (argc == 1 || argc > 3)
+    {
+        fprintf(stderr, "Incorrect Euler function specified.\n");
+        showUsage();
+        exit(-1);
+    }
+    // if help flag is suppplied, print help and then quit
+    for (i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-h") == 0)
+            showHelp();
+    }
+    // if user passes additional args, set. Otherwise set to -1
+    if (argc > 2)
+    {
+        *n = atoll(argv[2]);
+        if (*n < 0)
+            fprintf(stderr, "Functions do not take in negative input.\n");
+    }
+    else
+        *n = -1;
+    return atoi(argv[1]);
+}
+
+int main(int argc, char **argv)
+{
+    int prob;
+    long long int addlArgs;
+    prob = checkArgs(argc, argv, &addlArgs);
+    switch (prob)
+    {
+    case 1:
+        printf("Euler 1 is %d\n", euler1(addlArgs));
+        break;
+
+    case 2:
+        printf("Euler 2 is %d\n", euler2(addlArgs));
+        break;
+
+    case 3:
+        printf("Euler 3 is %ld\n", euler3(addlArgs));
+        break;
+
+    case 4:
+        printf("Euler 4 is %d\n", euler4(addlArgs));
+        break;
+
+    case 5:
+        printf("Euler 5 is %d\n", euler5(addlArgs));
+        break;
+
+    default:
+        printf("That problem is not available yet.");
+        break;
+    }
+}
